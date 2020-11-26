56bb805d1a4a1708106d9dd0094fd749bb85edea
Paola Garay <garay.angela@gmail.com>
2020-11-25 19:08:34 -0500

    Add jason-server

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const { GraphQLSchema, GraphQLInt, GraphQLString, GraphQLObjectType } = require('graphql');
-const _ = require('lodash')
+const axios = require('axios')
 
 
 const UserType = new GraphQLObjectType({
@@ -11,12 +11,8 @@ const UserType = new GraphQLObjectType({
     }
   });
 
-  const users = [
-        { id: '23', firstName: 'Bill', age: 20 },
-        { id: '47', firstName: 'Samantha', age: 21 } 
-    ];  
-      
 
+  
 
   const RootQuery= new GraphQLObjectType({
     name: 'RootQueryType',
@@ -25,11 +21,13 @@ const UserType = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString }},
             resolve(parentValue, args) {
-                return _.find(users, {id: args.id})
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                .then(resp => resp.data);
             }
         }
     }
   });
+
 
 
   module.exports = new GraphQLSchema({
