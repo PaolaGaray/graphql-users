96e30384e5599a2dcc10421f99932d2133143453
Paola Garay <garay.angela@gmail.com>
2020-11-25 20:08:16 -0500

    Add CompanyType in the GraphQL schema

--- schema/schema.js
+++ schema/schema.js
@@ -2,17 +2,33 @@ const { GraphQLSchema, GraphQLInt, GraphQLString, GraphQLObjectType } = require(
 const axios = require('axios')
 
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString} 
+    }
+});
+
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
-        age: {type: GraphQLInt} 
+        age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                .then(res => res.data);
+            }
+        }
     }
   });
 
 
-  
 
   const RootQuery= new GraphQLObjectType({
     name: 'RootQueryType',
